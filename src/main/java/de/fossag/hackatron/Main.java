c8c36c0e40fe43b8c874a4804bf4e00902c6959a
Jonas Zohren <git-pbkyr@jzohren.de>
2023-10-20 14:29:28 +0200

    fix: Use correct server url

--- src/main/java/de/fossag/hackatron/Main.java
+++ src/main/java/de/fossag/hackatron/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
     HackatronWrapper client = new HackatronWrapper(
-        "hackatron.de",
+        "game.hackatron.de",
         4000,
         new HackatronClient());
     client.run();
